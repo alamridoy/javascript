86696afef1d23fb10d5be5deb3364007ffc83838
alam ridoy <alamridoy7@gmail.com>
2023-08-22 00:02:56 +0600

    asynchronous learning

---
+++ 32.asynchronous.js
@@ -0,0 +1,17 @@
+const processOrder = (cuestomer) =>{
+    console.log("Process order for customer 1");
+
+    setTimeout(()=>{
+        console.log("cooking completed!!");
+        
+    },3000);
+
+    console.log("Order processed for customer 1");
+    
+}
+
+console.log("Order order for customer 1");
+processOrder()
+
+console.log("completed order for customer 1");
+
